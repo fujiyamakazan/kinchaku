a934aa5e5deae720f3402b7a05d9c246e968a1be
fujiyama <fujiyama@192.168.0.15>
2021-03-25 23:17:23 +0900

    home

--- src/main/java/net/nanisl/zabuton/util/file/XmlFileObj.java
+++ src/main/java/net/nanisl/zabuton/util/file/XmlFileObj.java
@@ -1,6 +1,7 @@
 package net.nanisl.zabuton.util.file;
 
 import java.io.File;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
@@ -8,6 +9,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.wicket.util.lang.Generics;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -22,23 +24,39 @@ public class XmlFileObj extends Utf8FileObj {
     /**
      * XPATHで値を取得します。
      * @param xpath XPATH
-     * @return
+     * @return 取得したテキスト
      */
-    public String getText(String xpath) {
+    public List<String> getText(String xpath) {
+        List<String> results = Generics.newArrayList();
         try {
             Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
             XPath xpathObj = XPathFactory.newInstance().newXPath();
             XPathExpression expr = xpathObj.compile(xpath);
             NodeList nodeList = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-            String text = "";
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Element e = (Element) nodeList.item(i);
-                text = e.getTextContent();
+                results.add(e.getTextContent());
             }
-            return text;
+            return results
+                ;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * XPATHで値を取得します。
+     * @param xpath XPATH
+     * @return 取得したテキストの先頭１件。取得できなければnull
+     */
+    public String getTextOne(String xpath) {
+        List<String> texts = getText(xpath);
+        if (texts.isEmpty()) {
+            return null;
+        } else {
+            return texts.get(0);
+        }
+
     }
 
 }
